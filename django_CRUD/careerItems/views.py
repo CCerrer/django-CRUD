e8a2a4d67e5f31f98d24e065643873f642c59659
CCerrer <mathferrer17ea@gmail.com>
2024-04-29 14:18:07 -0300

    feat: create Item view

--- django_CRUD/careerItems/views.py
+++ django_CRUD/careerItems/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
 
 # Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows items to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
