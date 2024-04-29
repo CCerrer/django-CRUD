71cb9846f3982f88c89289d86ed3fe138e0ed038
CCerrer <mathferrer17ea@gmail.com>
2024-04-29 18:33:43 -0300

    fix: fix patch endpoint restrictions

--- careerItems/serializers.py
+++ careerItems/serializers.py
@@ -5,3 +5,14 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'username', 'created_datetime', 'title', 'content']
+    
+    def create(self, validated_data):
+        return Item.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
+
+    
