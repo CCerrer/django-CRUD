a8bd524284831bb442f3f16807dcb3c2156d2ac6
CCerrer <mathferrer17ea@gmail.com>
2024-04-29 18:34:45 -0300

    test: implement unit tests

--- careerItems/tests.py
+++ careerItems/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from careerItems.models import Item
 
-# Create your tests here.
+class ItemTests(APITestCase):
+    def test_create_item(self):
+        url = reverse('careerItems:items-list')
+        data = {'username': 'fooUsername', 'title': 'fooTitle', 'content': 'fooContent'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.get().username, 'fooUsername')
+
+    def test_get_item(self):
+        item = Item.objects.create(username='fooUsername', title='fooTitle', content='fooContent')
+        url = reverse('careerItems:items-detail', kwargs={'pk': item.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], item.username)
+
+    def test_update_item(self):
+        item = Item.objects.create(username='fooUsername', title='fooTitle', content='fooContent')
+        url = reverse('careerItems:items-detail', kwargs={'pk': item.pk})
+        data = {'username': 'testUsername', 'title': 'testTitle', 'content': 'testContent'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Item.objects.get().username, 'testUsername')
+
+    def test_delete_item(self):
+        item = Item.objects.create(username='fooUsername', title='fooTitle', content='fooContent')
+        url = reverse('careerItems:items-detail', kwargs={'pk': item.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Item.objects.count(), 0)
--- careerItems/urls.py
+++ careerItems/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'items', views.ItemViewSet)
+router.register(r'items', views.ItemViewSet, basename='items')
+app_name = 'careerItems'
 
 urlpatterns = [
     path('', include(router.urls)),
